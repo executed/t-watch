cb45c9070893ca74957d0349c875df2b48c9f3bf
Tetiana Borina <denis.serbyn@gmail.com>
2019-03-05 16:17:54 +0200

    started implementing page parser

---
+++ src/main/java/com/devserbyn/twatch/service/parser/Parser.java
@@ -0,0 +1,31 @@
+package com.devserbyn.twatch.service.parser;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface Parser {
+
+    void processTextByZPath(String host, String path);
+
+    default Document getAndConnectDocument(String url) throws IOException {
+        return Jsoup.connect(url).get();
+    }
+
+    default Element getElementByZPath(Element element, List<String> ZPathList, int counter){
+        if(counter == ZPathList.size()) {
+            return element;
+        }
+        Element nextElement;
+        String node = ZPathList.get(counter);
+        if (node.contains("child")) {
+            nextElement = element.child(Integer.valueOf("" + node.charAt(7)));
+        }
+        else nextElement = element.select(ZPathList.get(counter)).first();
+        return getElementByZPath(nextElement, ZPathList, ++counter);
+    }
+}
