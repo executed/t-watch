1c64fa91640405cfd387e33ac3bdf490837b1b44
Tetiana Borina <denis.serbyn@gmail.com>
2019-03-09 19:10:01 +0200

    Start create currency parser

--- src/main/java/com/devserbyn/twatch/service/parser/Parser.java
+++ src/main/java/com/devserbyn/twatch/service/parser/Parser.java
@@ -1,20 +1,6 @@
 package com.devserbyn.twatch.service.parser;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import java.util.Optional;
-
 public interface Parser {
 
-    Optional<String> processZPath(String host, String zPath);
-
-    default String getElementValueByXPath(org.w3c.dom.Document doc, String xPath) {
-        try {
-            XPath xpath = XPathFactory.newInstance().newXPath();
-            return (String) xpath.evaluate(xPath, doc, XPathConstants.STRING);
-        } catch (Exception e) {
-            throw new RuntimeException("Error during processing XPath", e);
-        }
-    }
+    String processXPath(String url, String xPath);
 }
